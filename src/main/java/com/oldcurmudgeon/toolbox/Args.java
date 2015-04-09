d515af9d0163ceeb207c63e5e47c3952aee1c685
Paul <CaswellPaulA@AOL.com>
2015-04-09 09:10:57 +0100

    Args and Enums

---
+++ src/main/java/com/oldcurmudgeon/toolbox/Args.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2015 Paul Caswell.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.oldcurmudgeon.toolbox;
+
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author Paul Caswell
+ * @param <E>
+ */
+public class Args<E extends Enum<E> & Enums.ReverseLookup<E>> {
+  // The ons and offs.
+  private final Set<E> ons;
+  private final Set<E> offs;
+  private final Map<E, String> values;
+
+  /**
+   * An arg can be on (+x), off(-x), contain data (/x=data) or not present.
+   */
+  public enum State {
+    On, Off, Data, Absent;
+  }
+
+  /**
+   * All arg enums must reverse lookup.
+   *
+   * @param <E>
+   */
+  public interface Arg<E extends Enum<E>> extends Enums.ReverseLookup<E> {
+
+  }
+
+  /**
+   * Capture the command-line parameters.
+   *
+   * @param args
+   * @param of
+   */
+  public Args(String args[], Class<E> of) {
+    ons = EnumSet.<E>noneOf(of);
+    offs = EnumSet.<E>noneOf(of);
+    values = new EnumMap<>(of);
+    // Populate them.
+    for (String s : args) {
+      if (s != null && s.length() > 0) {
+        // Split on "=" if present.
+        String[] parts = s.split("=");
+        // What's the first character.
+        char first = s.charAt(0);
+        // What's the rest?
+        E arg = Enums.ReverseLookup.lookup(of, parts[0]);
+        if (arg == null) {
+          arg = Enums.ReverseLookup.lookup(of, parts[0].substring(1));
+        }
+        if (arg != null) {
+          switch (first) {
+            case '+':
+              ons.add(arg);
+              break;
+            case '-':
+              offs.add(arg);
+              break;
+            case '/':
+              values.put(arg, parts[1] != null ? parts[1] : "");
+              break;
+            default:
+              // No decoration - if theres an equals then it's a value, otherwise it's an on.
+              if (parts.length > 1) {
+                values.put(arg, parts[1]);
+              } else {
+                ons.add(arg);
+              }
+          }
+        } else {
+          System.err.println("Arg '" + s + "' invalid or unrecognised.");
+        }
+
+      }
+    }
+  }
+
+  /**
+   * Is this arg on/off/not present.
+   *
+   * @param a
+   * @return
+   */
+  public Set<State> is(E a) {
+    Set<State> state = EnumSet.noneOf(State.class);
+    if (ons.contains(a)) {
+      state.add(State.On);
+    }
+    if (offs.contains(a)) {
+      state.add(State.Off);
+    }
+    return state;
+  }
+
+  public enum TestArgs implements Arg<TestArgs> {
+    Arg1, Arg2, Arg3, Arg4, Arg5;
+  }
+
+  public static void main(String args[]) {
+    try {
+      Args a = new Args(new String[]{"Arg1", "Arg2", "/Arg3=10", "+Arg4", "-Arg5"}, TestArgs.class);
+    } catch (Throwable t) {
+      t.printStackTrace(System.err);
+    }
+  }
+
+}
---
+++ src/main/java/com/oldcurmudgeon/toolbox/Enums.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2015 Paul Caswell.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.oldcurmudgeon.toolbox;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutionException;
+
+/**
+ *
+ * @author Paul Caswell
+ */
+public class Enums {
+
+  // ALL enums have a name.
+  public interface HasName {
+
+    public String name();
+
+  }
+
+  public interface PoliteEnum extends HasName {
+
+    default String politeName() {
+      return name().replace("_", " ");
+    }
+
+  }
+
+  public interface Lookup<P, Q> {
+
+    public Q lookup(P p) throws Exception;
+
+  }
+
+  public interface ReverseLookup<E extends Enum<E>> extends Lookup<String, E> {
+
+    // Map of all classes that have lookups.
+    ConcurrentMap<Class, Map<String, Enum>> lookups = new ConcurrentHashMap<>();
+
+    // What I need from the Enum.
+    Class<E> getDeclaringClass();
+
+    public static <E extends Enum<E>> E lookup(Class<E> c, String name) {
+      // Get the map - creating it if necessary.
+      final Map<String, Enum> lookup = lookups.computeIfAbsent(c, k -> new HashMap<>());
+      if (lookup.isEmpty()) {
+        // Populate it.
+        for (E e : c.getEnumConstants()) {
+          lookup.put(e.name(), e);
+          // Also add the polite versions if they are there.
+          if (e instanceof PoliteEnum) {
+            String politeName = ((PoliteEnum) e).politeName();
+            lookup.put(politeName, e);
+            lookup.put(politeName.toLowerCase(), e);
+            lookup.put(politeName.toUpperCase(), e);
+
+          }
+        }
+      }
+      // Look it up.
+      return c.cast(lookup.get(name));
+    }
+
+    @Override
+    default E lookup(String name) throws InterruptedException, ExecutionException {
+      // Use the static one.
+      return lookup(getDeclaringClass(), name);
+    }
+
+  }
+
+  // Use the above interfaces to add to the enum.
+  public enum X implements PoliteEnum, ReverseLookup<X> {
+
+    A_For_Ism,
+    B_For_Mutton,
+    C_Forth_Highlanders;
+  }
+
+  public void test() throws InterruptedException, ExecutionException {
+    System.out.println("Hello");
+    System.out.println(X.C_Forth_Highlanders.lookup("A For Ism"));
+    System.out.println(ReverseLookup.lookup(X.class, "B For Mutton"));
+  }
+
+  public static void main(String args[]) {
+    try {
+      new Enums().test();
+    } catch (Throwable t) {
+      t.printStackTrace(System.err);
+    }
+  }
+
+}
