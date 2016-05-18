25d0696084e9e6c773b4d35ba7057186665a10a4
pcaswell <pcaswell@PCASWELL-GB.uk.oracle.com>
2016-05-18 17:24:49 +0100

    Holder

--- src/main/java/com/oldcurmudgeon/toolbox/pipe/Pipe.java
+++ src/main/java/com/oldcurmudgeon/toolbox/pipe/Pipe.java
@@ -1,11 +1,16 @@
 package com.oldcurmudgeon.toolbox.pipe;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -18,55 +23,241 @@ import java.util.function.Supplier;
  */
 public class Pipe<T> implements Input<T>, Output<T> {
 
-  // Holding area for the things.
-  private BlockingQueue<T> q = new ArrayBlockingQueue(1);
+  interface Holder<T> {
 
-  /**
-   * Consumer should take as little time as possible.
-   */
-  private Consumer<T> consumer = Exceptions.rethrowConsumer(t -> q.add(t));
+    public T get();
 
-  /**
-   * Producer obtains the result - performing calculations as necessary.
-   */
-  private Supplier<T> producer = Exceptions.rethrowSupplier(() -> q.remove());
+    public void put(T it);
+  }
 
-  /**
-   * Closed for writing - throws an exception if you try to write to a joined pipe.
-   *
-   * Please remember to only write to the first pipe.
-   */
-  private static final Consumer CLOSED = Exceptions.rethrowConsumer(t -> {
-    throw new PipeException("Attempt to write to a joined pipe.");
-  });
+  static class HoldBlockingQueue<T> implements Holder<T> {
 
-  /**
-   * Simple pipe.
-   */
-  public Pipe() {
+    // Holding area for the things.
+    private BlockingQueue<T> q = new ArrayBlockingQueue(1);
+
+    /**
+     * Consumer should take as little time as possible.
+     *
+     * Just push it into the queue.
+     */
+    private Consumer<T> consumer = Exceptions.rethrowConsumer(q::add);
+
+    /**
+     * Producer obtains the result - performing calculations as necessary.
+     *
+     * Pull it from the queue.
+     */
+    private Supplier<T> producer = Exceptions.rethrowSupplier(q::remove);
+
+    /**
+     * Transform Holder functions to BlockingQueue functions.
+     */
+    @Override
+    public T get() {
+      return producer.get();
+    }
+
+    @Override
+    public void put(T it) {
+      consumer.accept(it);
+    }
+
+    public static <T> Holder<T> make(Pipe p) {
+      // Ignore the pipe.
+      return new HoldBlockingQueue<>();
+    }
+  }
+
+  static class HoldAtomic<T> implements Holder<T> {
+
+    // Holding area for the things.
+    private AtomicReference<T> r = new AtomicReference<>(null);
+
+    /**
+     * Stick it into the Atomic.
+     */
+    private Consumer<T> consumer = r::set;
+
+    /**
+     * Pull it out.
+     */
+    private Supplier<T> producer = r::get;
+
+    /**
+     * Transform Holder functions to BlockingQueue functions.
+     */
+    @Override
+    public T get() {
+      return producer.get();
+    }
+
+    @Override
+    public void put(T it) {
+      consumer.accept(it);
+    }
+
+    public static <T> Holder<T> make(Pipe p) {
+      // Ignore the pipe.
+      return new HoldAtomic<>();
+    }
+  }
+
+  static class HoldDelegate<T> implements Holder<T> {
+
+    /**
+     * Pass it on.
+     */
+    private Consumer<T> consumer;
+
+    /**
+     * Pull it out.
+     */
+    private Supplier<T> producer;
+
+    public HoldDelegate(Pipe<T> joinTo) {
+      // Delegate to the joined pipe.
+      consumer = joinTo::put;
+      producer = joinTo::get;
+    }
+
+    /**
+     * Delegate all.
+     */
+    @Override
+    public T get() {
+      return producer.get();
+    }
+
+    @Override
+    public void put(T it) {
+      consumer.accept(it);
+    }
+
+    public static <T> Holder<T> make(Pipe<T> p) {
+      // Ignore the pipe.
+      return new HoldDelegate<>(p);
+    }
   }
 
   /**
-   * Special producer/consumer/functions set.
+   * Pick a holding strategy to use.
+   *
+   * Each is a Holder factory
    */
-  private Pipe(Supplier<T> producer,
-          Consumer<T> consumer,
+  enum HoldingStrategy {
+    /**
+     * Hold nothing.
+     */
+    None(new Function<Pipe, Holder>() {
+      // We are stateless so we only need one supplier.
+      final Holder NOTHING = new Holder() {
+        /**
+         * Always give null.
+         */
+        @Override
+        public Object get() {
+          return null;
+        }
+
+        /**
+         * Black hole.
+         */
+        @Override
+        public void put(Object it) {
+        }
+
+      };
+
+      /**
+       * Give them a Holder that uses this strategy.
+       */
+      @Override
+      public Holder apply(Pipe t) {
+        // Always give them the same one.
+        return NOTHING;
+      }
+    }),
+    /**
+     * Writing overwrites the previous.
+     *
+     * Reading reads the current value.
+     */
+    PassThrough(HoldAtomic::make),
+    /**
+     * Writing blocks until space available.
+     *
+     * Reading blocks until something is written.
+     */
+    Block(HoldBlockingQueue::make),
+    /**
+     * Forward all requests up the chain.
+     */
+    Delegate(HoldDelegate::make);
+
+    /**
+     * Keeps track of the maker that makes the Holders.
+     */
+    final Function<Pipe, Holder> maker;
+
+    public <T> Holder<T> holder() {
+      return maker.<T>apply(NOPIPE);
+    }
+
+    public <T> Holder<T> holder(Pipe<T> from) {
+      return maker.<T>apply(from);
+    }
+
+    HoldingStrategy(Function<Pipe, Holder> maker) {
+      this.maker = maker;
+    }
+  }
+
+  // A null pipe
+  private static final Pipe NOPIPE = new Pipe();
+  /**
+   * What to do to the data at pull time.
+   *
+   * Initially do nothing.
+   */
+  private Function<T, T> function = i -> i;
+  /**
+   * I must have a Holder to hold a T while in transit.
+   */
+  final Holder<T> it;
+
+  /**
+   * Use a strategy.
+   */
+  private Pipe(HoldingStrategy strategy,
           List<String> functions,
           List<List<String>> otherFunctions) {
-    this.producer = producer;
-    this.consumer = consumer;
+    // Tell the strategy to make the holder.
+    it = strategy.holder();
     this.previousFunctions.addAll(otherFunctions);
     this.previousFunctions.add(functions);
   }
 
+  private Pipe(HoldingStrategy strategy) {
+    this(strategy, Collections.EMPTY_LIST, Collections.EMPTY_LIST);
+  }
+
   /**
-   * Join me onto an existing pipe of my same type.
+   * Default to no holding at all.
+   */
+  public Pipe() {
+    // Default to None.
+    this(HoldingStrategy.None);
+  }
+
+  /**
+   * Join me onto the end of an existing pipe of my same type.
    *
    * @param pipe - The pipe to feed off.
    */
   private Pipe(Pipe<T> pipe) {
-    // Chain from it. Close my consumer.
-    this(() -> pipe.get(), CLOSED, pipe.functions, pipe.previousFunctions);
+    // Chain from it.
+    this(HoldingStrategy.PassThrough, pipe.functions, pipe.previousFunctions);
+    // TODO - Close the put pipe. this(() -> pipe.get(), CLOSED, );
   }
 
   /**
@@ -90,25 +281,20 @@ public class Pipe<T> implements Input<T>, Output<T> {
   @Override
   public Pipe<T> put(T datum) {
     // Consume it.
-    consumer.accept(datum);
+    it.put(datum);
     return this;
   }
 
   /**
    * Pull whatever is available out of the pipe.
    *
-   * Does nothing by default. Add flows and filters to change that.
-   *
    * @return - The received data.
    */
   @Override
   public T get() {
     // Get it out of the buffer and perform the function on it.
-    return function.apply(producer.get());
+    return function.apply(it.get());
   }
-
-  // Initially do nothing.
-  private Function<T, T> function = i -> i;
 
   /**
    * Add another function to the chain.
@@ -127,9 +313,20 @@ public class Pipe<T> implements Input<T>, Output<T> {
   }
 
   /**
-   * Join a new pipe on the end of me.
+   * Add another function to the chain.
    *
-   * Used by `join` so private.
+   * @param function - What more to do.
+   * @param c
+   * @param name - Descriptive for it.
+   * @return - this.
+   */
+  public Pipe<T> addFunction(BiFunction<T, T, T> function, T c, String name) {
+    // Wrap c in a lambda.
+    return addFunction((T t) -> function.apply(t, c), name);
+  }
+
+  /**
+   * Join a new pipe on the end of me.
    *
    * @param <U> - The new pipe type.
    * @param map
@@ -204,6 +401,45 @@ public class Pipe<T> implements Input<T>, Output<T> {
 
   }
 
+  /**
+   * Utility Math functions.
+   */
+  public static class Math {
+
+    public static final BigDecimal PI = BigDecimal.valueOf(java.lang.Math.PI);
+
+    public static BigInteger mul(BigInteger a, BigInteger b) {
+      return a.multiply(b);
+    }
+
+    public static BigInteger div(BigInteger a, BigInteger b) {
+      return a.divide(b);
+    }
+
+    public static BigInteger add(BigInteger a, BigInteger b) {
+      return a.add(b);
+    }
+
+    public static BigInteger sub(BigInteger a, BigInteger b) {
+      return a.subtract(b);
+    }
+
+    public static BigInteger pow(BigInteger a, BigInteger b) {
+      return a.pow(b.intValue());
+    }
+
+  }
+  /**
+   * Closed for writing - throws an exception if you try to write to a joined pipe.
+   *
+   * Please remember to only write to the first pipe.
+   *
+   * This is attached to the consumer of the downstream pipe when it is added to an existing one.
+   */
+  private static final Consumer CLOSED = Exceptions.rethrowConsumer(t -> {
+    throw new PipeException("Attempt to write to a joined pipe.");
+  });
+
   // !!!! TESTING !!!!
   public void test() {
     System.out.println("--------");
@@ -211,8 +447,23 @@ public class Pipe<T> implements Input<T>, Output<T> {
     test2();
     test3();
     test4();
+    test5();
     System.out.println("--------");
     //Stream.of(1,2,3).map(mapper)
+  }
+
+  private void test5() {
+    System.out.println("-- 05 -- \"Times Table\" pipe.");
+    Pipe<BigInteger> timesTable[] = new Pipe[10];
+    for (int i = 0; i < timesTable.length; i++) {
+      final int c = i;
+      timesTable[i] = new Pipe<BigInteger>()
+              .addFunction(Math::mul, BigInteger.valueOf(i), "Times " + i);
+    }
+    for (int i = 0; i < timesTable.length; i++) {
+      timesTable[i].put(BigInteger.TEN);
+      System.out.println("t(" + i + ") = " + timesTable[i].get());
+    }
   }
 
   private void test4() {
@@ -221,11 +472,11 @@ public class Pipe<T> implements Input<T>, Output<T> {
      */
     System.out.println("-- 04 -- \"Failing\" pipe.");
     // Make a pipe to Math.cbrt (cube root).
-    Pipe<Double> cbrt = new Pipe<Double>()
+    Pipe<BigInteger> cbrt = new Pipe<BigInteger>()
             // Cube root
-            .addFunction(Math::cbrt, "cbrt");
+            .addFunction(BigInteger::abs, "cbrt");
     // Joint it to stringify.
-    Pipe<String> strings = new Pipe<>(cbrt, (d) -> Double.toString(d));
+    Pipe<String> strings = new Pipe<>(cbrt, BigInteger::toString);
     // Should fail because cbrt feeds strings.
     try {
       strings.put("Ooops!");
@@ -240,7 +491,7 @@ public class Pipe<T> implements Input<T>, Output<T> {
      */
     System.out.println("-- 03 -- \"Joint\" pipe.");
     // Make a pipe to Math.cbrt (cube root).
-    Pipe<Double> cbrt = new Pipe<Double>()
+    Pipe<BigInteger> cbrt = new Pipe<BigInteger>()
             // Cube root
             .addFunction(Math::cbrt, "cbrt");
     // What does it look like?
@@ -271,11 +522,11 @@ public class Pipe<T> implements Input<T>, Output<T> {
             // Cube root
             .addFunction(Math::cbrt, "cbrt")
             // e^x - 1
-            .addFunction(Math::expm1, "expmt1");
+            .addFunction(java.lang.Math::expm1, "expmt1");
     // Hack the pipe to call Math.stuff.
     // Pull out the result.
     Double got = eToTheCubeRootOfPiMinusOne.put(Math.PI).get();
-    System.out.println("expected " + (Math.expm1(Math.cbrt(Math.PI))));
+    System.out.println("expected " + (java.lang.Math.expm1(java.lang.Math.cbrt(Math.PI))));
     System.out.println("Pipe: " + eToTheCubeRootOfPiMinusOne + " got      " + got);
   }
 
